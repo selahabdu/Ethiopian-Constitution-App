d3fb145bec063b3edc3aa882434ba1caf97be55e
selahabdu <zselah34@gmail.com>
2022-06-24 01:33:02 +0300

    Action listener created for exit_option_item with Alert Box

--- app/src/main/java/com/selah/constitution/Home.java
+++ app/src/main/java/com/selah/constitution/Home.java
@@ -1,10 +1,15 @@
 package com.selah.constitution;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -19,7 +24,42 @@ public class Home extends AppCompatActivity {
         menuInflater.inflate(R.menu.option_menu,menu);
         return true;
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.item_exit:
+                Toast.makeText(getApplicationContext(), "File Item Selected", Toast.LENGTH_SHORT).show();
+                exitApp();
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+    public void exitApp(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(Home.this);
+        builder.setMessage("Do You Want To Exit ?");
+        builder.setCancelable(true);
+        //
+        builder.setNegativeButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        //
+        builder.setPositiveButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
 
 
 
