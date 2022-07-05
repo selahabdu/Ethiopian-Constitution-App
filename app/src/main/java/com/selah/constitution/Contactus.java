85e8e804fdff1b932adab326c1044fee5360531a
Murad <muradabdlsemed44@gmail.com>
2022-07-05 23:08:15 +0300

    create  abutton to aboutus page to contuctas page

--- app/src/main/java/com/selah/constitution/Contactus.java
+++ app/src/main/java/com/selah/constitution/Contactus.java
@@ -3,12 +3,17 @@ package com.selah.constitution;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Contactus extends AppCompatActivity {
-
+EditText nametxt,emailtxt;
+Button sendbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactus);
+        nametxt=findViewById(R.id.textname);
+        emailtxt=findViewById(R.id.emailtext);
     }
 }
--- app/src/main/java/com/selah/constitution/aboutus.java
+++ app/src/main/java/com/selah/constitution/aboutus.java
@@ -2,13 +2,25 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class aboutus extends AppCompatActivity {
-
+  public class aboutus extends AppCompatActivity {
+Button contactbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
+        contactbutton=findViewById(R.id.btncontact);
+        contactbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contactIntent=new Intent(getApplicationContext(),Contactus.class);
+                startActivity(contactIntent);
+            }
+        });
+
     }
 }
