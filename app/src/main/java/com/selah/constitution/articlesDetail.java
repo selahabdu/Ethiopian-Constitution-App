4c229ffe7575fbedf67fc5aa8b7859df09c85656
selahabdu <zselah34@gmail.com>
2022-07-10 14:42:59 +0300

    new separate menu created for the articlesDetail, and the menu of the home page modified, icon added for language menu item

--- app/src/main/java/com/selah/constitution/articlesDetail.java
+++ app/src/main/java/com/selah/constitution/articlesDetail.java
@@ -22,7 +22,7 @@ public class articlesDetail extends AppCompatActivity {
     SharedPrefs sharedPreferences;
     SeekBar textSizeSeekBar;
     int textSizeValue;
-    ListAdapter articleListAdapterAdapter;
+    TextView article_name,article_description,article_detail;
     //language preference
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -35,13 +35,11 @@ public class articlesDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_articles_detail);
-        textSizeSeekBar = findViewById(R.id.textSizeSeekBar);
         Intent intent = getIntent();
         String name = intent.getStringExtra("title");
         String description = intent.getStringExtra("description");
         String detail = intent.getStringExtra("detail");
 
-        TextView article_name,article_description,article_detail;
         article_name = findViewById(R.id.article_layout_title);
         article_description = findViewById(R.id.article_layout_description);
         article_detail = findViewById(R.id.article_layout_detail);
@@ -49,7 +47,7 @@ public class articlesDetail extends AppCompatActivity {
         article_name.setText(name);
         article_description .setText(description);
         article_detail .setText(detail);
-
+        textSizeSeekBar = findViewById(R.id.textSizeSeekBar);
         textSizeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -70,16 +68,22 @@ public class articlesDetail extends AppCompatActivity {
 
             }
         });
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater =  getMenuInflater();
-        menuInflater.inflate(R.menu.option_menu,menu);
+        menuInflater.inflate(R.menu.menu_for_article_detail,menu);
         return true;
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
+            case R.id.item_text_size:
+            {
+
+            }
+
             case R.id.item_amharic:
             {
                 sharedPreferences.setLocale("am");
@@ -108,5 +112,8 @@ public class articlesDetail extends AppCompatActivity {
             default:
                 return super.onContextItemSelected(item);
         }
+    }
+    public void textSize(){
+
     }
 }
